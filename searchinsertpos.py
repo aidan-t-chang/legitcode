f5c3d653183ab49297a14b04d5eeb2d2111f653f
plinygg <119556552+plinygg@users.noreply.github.com>
2024-03-25 16:19:17 +0000

    tried searchinsert using binary search

--- searchinsertpos.py
+++ searchinsertpos.py
@@ -5,8 +5,43 @@
 
 # binary search must use!!
 def searchInsert(nums, target):
-    counter = 0
+    min = 0
+    max = len(nums) - 1
+    avg = (min + max) // 2
     if target in nums:
-        return nums.find(target)
-    else:
-        print(counter)
+        while True:
+            if nums[avg] == target:
+                return avg
+            if nums[avg] < target:
+                min = avg + 1
+                avg = (min + max) // 2
+            if nums[avg] > target:
+                max = avg - 1
+                avg = (min + max) // 2
+    while True:
+        if nums[0] >= target:
+            return 0
+        if nums[max - 1] <= target:
+            return len(nums)
+        if nums[avg - 1] < target < nums[avg] + 1:
+            return avg
+        if nums[avg] < target:
+            min = avg + 1
+            avg = (min + max) // 2
+        if nums[avg] > target:
+            max = avg - 1
+            avg = (min + max) // 2
+
+def binary2(nums, target):
+    min = 0
+    max = len(nums) - 1
+    avg = (min + max) // 2
+    return nums
+
+print('return 4', searchInsert([1,3,5,6], 7))
+print('return 1', searchInsert([1,3,5,6], 2))
+print('return 2', searchInsert([1,3,5,6], 5))
+print('return 0', searchInsert([1,3,5,6], 0))
+print('return 1', searchInsert([1,3], 2))
+print('return 2', searchInsert([1,4,6,7,8,9], 6))
+print('return 2', searchInsert([1,2,4,5,6], 3))
