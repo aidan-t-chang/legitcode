8ecdf566490661b129914e17d97459c4deef2f8a
plinygg <119556552+plinygg@users.noreply.github.com>
2024-03-26 17:26:47 +0000

    solved plusone

--- plusone.py
+++ plusone.py
@@ -4,3 +4,15 @@
 # The large integer does not contain any leading 0's.
 # Increment the large integer by one and return the resulting array of digits.
 
+def plusone(digits):
+    exponent = 0
+    inte = 0
+    for i in digits[::-1]:
+        inte += i * (10**exponent)
+        exponent += 1
+    splitagain = [int(n) for n in str(inte+1)]
+    return splitagain
+
+print(plusone([1,2,3]))
+print(plusone([9]))
+    
