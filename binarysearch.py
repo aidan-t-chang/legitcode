26146f6ba8e43347fbd34fcc725b7e0bc666e3de
plinygg <119556552+plinygg@users.noreply.github.com>
2024-03-29 21:57:07 +0000

    solved binary search

--- binarysearch.py
+++ binarysearch.py
@@ -1,14 +1,18 @@
+# write a binary search.
+
 def binarySearch(nums, target):
     min = 0
     max = len(nums)
-    avg = (min + max) // 2
+    mid = (min + max) // 2
     while True:
-        if nums[avg] == target:
-            return avg
-        if nums[avg] < target:
-            min = avg + 1
-            avg = (min + max) // 2
+        if target not in nums:
+            return -1
+        if nums[mid] == target:
+            return mid
+        if nums[mid] < target:
+            min = mid + 1
+            mid = (min + max) // 2
         else:
-            max = avg - 1
-            avg = (min + max) // 2
-    return avg
+            max = mid - 1
+            mid = (min + max) // 2
+    return mid
