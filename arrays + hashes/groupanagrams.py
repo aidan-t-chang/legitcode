15103ae7e34197b40a6564fe3a3135452be91802
plinygg <119556552+plinygg@users.noreply.github.com>
2024-04-04 18:04:29 +0000

    solved leetcode 49

--- arrays + hashes/groupanagrams.py
+++ arrays + hashes/groupanagrams.py
@@ -1,3 +1,13 @@
 # Given an array of strings strs, group the anagrams together. 
 # You can return the answer in any order.
 
+from collections import defaultdict 
+
+def groupAnagrams(strs):
+    ret = defaultdict(list)
+    for str in strs:
+        count = [0] * 26
+        for c in str:
+            count[ord(c) - ord('a')] += 1
+        ret[tuple(count)].append(str)
+    return ret.values()
