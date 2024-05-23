c2d9fff86e01421cd0c6a6a9df44453ebb3d3849
atchang <aidan.chang@outlook.com>
2024-05-23 13:30:02 -0500

    solved leetcode 183

---
+++ 30 days of pandas/customersneverorder.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def find_customers(customers, orders):
+
+    #boolean indexing to filter rows from the "customers" dataframe
+    df = customers[~customers['id'].isin(orders['customerId'])]
+    
+    #build the dataframe
+    df = df[['name']].rename(columns={'name': 'Customers'})
+    return df
