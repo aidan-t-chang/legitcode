82c014e634fa69e9e3fb8007d2084e6b5e835366
plinygg <aidan.chang@outlook.com>
2024-06-13 17:51:57 -0500

    fixed indentation error

--- trees/binarytreelevelordertraversal.py
+++ trees/binarytreelevelordertraversal.py
@@ -24,7 +24,7 @@ class Solution:
                     level.append(node.val)
                     q.append(node.left)
                     q.append(node.right)
-            if level:
-                res.append(level)
+        if level:
+            res.append(level)
         return res
             
