13ff51a796a1bee5e4a187b428f9f216f3ef4f7b
atchang <aidan.chang@outlook.com>
2024-06-16 21:55:18 -0500

    solved leetcode 230

---
+++ trees/kthsmallestelementinBST.py
@@ -0,0 +1,19 @@
+# Given the root of a binary search tree, 
+# and an integer k, return the kth smallest value (1-indexed) 
+# of all the values of the nodes in the tree.
+
+class Solution:
+    def kthSmallest(self, root, k: int) -> int:
+        n = 0
+        stack = []
+        cur = root
+
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            n += 1
+            if n == k:
+                return cur.val
+            cur = cur.right
