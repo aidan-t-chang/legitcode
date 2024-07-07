5195b4103e67b288dc87fb41cab0ac7c268ce971
atchang <aidan.chang@outlook.com>
2024-07-06 22:56:25 -0500

    solved daily problem 7/7/24

---
+++ daily problems/07.07-waterbottles.py
@@ -0,0 +1,20 @@
+# There are numBottles water bottles that are initially full of water. 
+# You can exchange numExchange empty water bottles from the market 
+# with one full water bottle.
+
+# The operation of drinking a full water bottle turns it into an empty bottle.
+
+# Given the two integers numBottles and numExchange, 
+# return the maximum number of water bottles you can drink.
+
+
+class Solution:
+    def numWaterBottles(self, numBottles, numExchange):
+        res = numBottles # always possible you can drink all available bottles at start
+        numEmpty = numBottles 
+        while numBottles >= numExchange:
+            newNew = numBottles // numExchange # number of new full bottles
+            remainder = numBottles % numExchange # remainder of empty bottles after exchange
+            res += newNew # update amount of full bottles
+            numBottles = newNew + remainder# update the amount of bottles left
+        return res
