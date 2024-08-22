7ca6d2a3b77eaf5a5ecdd30646af8110b96fa1ae
plinygg <aidan.chang@outlook.com>
2024-08-21 21:23:14 -0500

    solved daily problem 8/22/24

---
+++ daily problems/08.22-numbercomplement.py
@@ -0,0 +1,20 @@
+# The complement of an integer is the integer you get when you flip 
+# all the 0's to 1's and all the 1's to 0's in its binary representation.
+
+# For example, The integer 5 is "101" in binary and its complement is 
+# "010" which is the integer 2.
+# Given an integer num, return its complement.
+
+class Solution:
+    def findComplement(self, num: int) -> int:
+        ninb =  format(num, 'b')
+
+        res = 0
+        for n in ninb:
+            res *= 10
+            if int(n) == 0:
+                res += 1
+        
+        res2 = int(str(res), 2)
+
+        return res2
