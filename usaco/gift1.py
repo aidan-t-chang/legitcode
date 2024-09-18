428072ce5462be65a78e983385ea80bd19e4eaf9
plinygg <aidan.chang@outlook.com>
2024-09-18 01:35:39 -0500

    solved usaco gift1

--- usaco/gift1.py
+++ usaco/gift1.py
@@ -1,7 +1,29 @@
 """
 ID: your_id_here
 LANG: PYTHON3
-TASK: word
+TASK: gift1
 """
-fin = open('word.in', 'r')
-fout = open('word.out', 'w')
+
+fin = open('gift1.in','r')
+np = int(fin.readline().strip())
+dictOfMoney = { fin.readline().strip() : 0 for i in range(np) }
+
+while(True):
+	giver = fin.readline().strip()
+	if(giver==""):
+		break
+	amount, divided = map(int, fin.readline().strip().split())
+	receivers = [fin.readline().strip() for i in range(divided)]
+
+	try:
+		quotient, remainder = divmod(amount,divided)
+	except:
+		quotient = remainder = 0
+
+	for receiver in receivers:
+		dictOfMoney[receiver] += quotient
+	dictOfMoney[giver] += -amount + remainder
+
+with open('gift1.out','w') as fout:
+	for name, money in dictOfMoney.items():
+		fout.write(f"{name} {money}\n")
