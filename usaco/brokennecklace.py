693d07333be827f76fc2d4f0d844a9f00bfa074a
plinygg <aidan.chang@outlook.com>
2024-10-15 18:08:27 -0500

    broken necklace still unsolved

--- usaco/brokennecklace.py
+++ usaco/brokennecklace.py
@@ -10,5 +10,11 @@ beads = str(fin.readline())
 
 
 # sliding window - longest common substring including w?
+# two pass; one for all r, one for all b?
+# basically longest common substring except with just one bead color
 
-l, r = 0, 0
+l = 0
+# first iteration will be for red beads
+for r in range(len(beads)):
+    if beads[r] == 'b':
+        pass
