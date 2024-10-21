7f11f16d9a8cae509f4a5ac53a2a60cfd96e0d31
plinygg <aidan.chang@outlook.com>
2024-10-21 17:44:47 -0500

    hoofpaperscissors unsolved

--- usaco/usaco guide/silver/prefix sums/hoofpaperscissors.py
+++ usaco/usaco guide/silver/prefix sums/hoofpaperscissors.py
@@ -6,9 +6,28 @@ TASK: hps
 
 
 fin = open('hps.in', 'r')
+N = int(fin.readline())
+arr = []
+for i in range(N):
+    temp = fin.readline()
+    arr.append(temp)
+    
+h, p, s = [0], [0], [0]
 
-
-
+for n in arr:
+    if n == "H":
+        h.append(h[-1]+1)
+        p.append(p[-1])
+        s.append(s[-1])
+    elif n == "P":
+        p.append(p[-1]+1)
+        h.append(h[-1])
+        s.append(s[-1])
+    elif n == "S":
+        s.append(s[-1]+1)
+        p.append(p[-1])
+        h.append(h[-1])
+    
 
 with open('hps.out', 'w') as fout:
     pass
