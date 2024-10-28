0e8b9e1bc051a4577767c978aa00db1962e70e75
plinygg <aidan.chang@outlook.com>
2024-10-28 18:58:40 -0500

    (not) solved daily problem 10/28/24

---
+++ daily problems/10.28-longestsquarestreakinanarrray.py
@@ -0,0 +1,30 @@
+# You are given an integer array nums. 
+# A subsequence of nums is called a square streak if:
+
+# The length of the subsequence is at least 2, and
+# after sorting the subsequence, each element (except the first element) 
+# is the square of the previous number.
+# Return the length of the longest square streak in nums, 
+# or return -1 if there is no square streak.
+
+# A subsequence is an array that can be derived from another array by deleting some 
+# or no elements without changing the order of the remaining elements.
+from math import isqrt
+
+
+class Solution:
+    def longestSquareStreak(self, nums):
+        mp = {}
+        nums.sort()
+        res = -1
+
+        for num in nums:
+            sqrt = isqrt(num)
+
+            if sqrt * sqrt == num and sqrt in mp:
+                mp[num] = mp[sqrt] + 1
+                res = max(res, mp[num])
+            else:
+                mp[num] = 1
+
+        return res
