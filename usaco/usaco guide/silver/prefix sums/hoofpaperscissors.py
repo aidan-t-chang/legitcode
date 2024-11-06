5257865a15c46ef5dfe7e4056e214d4c34eb98dc
plinygg <aidan.chang@outlook.com>
2024-11-05 23:19:38 -0600

    these open conventions are screwing me over (solved usaco hoof paper scissors)

--- usaco/usaco guide/silver/prefix sums/hoofpaperscissors.py
+++ usaco/usaco guide/silver/prefix sums/hoofpaperscissors.py
@@ -4,30 +4,32 @@ LANG: PYTHON3
 TASK: hps
 """
 
+read = open("hps.in", "r").readline
+write = open("hps.out", "w").write
 
-fin = open('hps.in', 'r')
-N = int(fin.readline())
-arr = []
-for i in range(N):
-    temp = fin.readline()
-    arr.append(temp)
+n, ans = int(read()), 0
+
+pref = [[0 for _ in range(n)] for _ in range(3)]
+
+H, P, S = 0, 1, 2
+h, p, s = 0, 0, 0
+
+for i in range(n):
+    ch = read()[0]
+    if ch == 'H':
+        h += 1
+    elif ch == 'P':
+        p += 1
+    elif ch == 'S':
+        s += 1
+    pref[H][i] = h
+    pref[P][i] = p
+    pref[S][i] = s
     
-h, p, s = [0], [0], [0]
-
-for n in arr:
-    if n == "H":
-        h.append(h[-1]+1)
-        p.append(p[-1])
-        s.append(s[-1])
-    elif n == "P":
-        p.append(p[-1]+1)
-        h.append(h[-1])
-        s.append(s[-1])
-    elif n == "S":
-        s.append(s[-1]+1)
-        p.append(p[-1])
-        h.append(h[-1])
-    
-
-with open('hps.out', 'w') as fout:
-    pass
+for i in range(n):
+    max_left, max_right = 0, 0
+    for j in range(3):
+        max_left = max(max_left, pref[j][i])
+        max_right = max(max_right, pref[j][n - 1] - pref[j][i])
+    ans = max(ans, max_left + max_right)
+write(str(ans))
