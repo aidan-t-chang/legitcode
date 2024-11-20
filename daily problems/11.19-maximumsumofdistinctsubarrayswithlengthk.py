513a5db42eac2acd23dc181351975169ed57234d
plinygg <aidan.chang@outlook.com>
2024-11-19 22:24:57 -0600

    (not) solved daily problem 11/19/24

---
+++ daily problems/11.19-maximumsumofdistinctsubarrayswithlengthk.py
@@ -0,0 +1,30 @@
+# You are given an integer array nums and an integer k. Find the maximum subarray sum of all the subarrays of nums that meet the following conditions:
+
+# The length of the subarray is k, and
+# All the elements of the subarray are distinct.
+# Return the maximum subarray sum of all the subarrays that meet the conditions. If no subarray meets the conditions, return 0.
+
+# A subarray is a contiguous non-empty sequence of elements within an array.
+
+
+class Solution:
+    def maximumSubarraySum(self, nums, k: int) -> int:
+        ans = 0
+        current_sum = 0
+        begin = 0
+        end = 0
+        num_to_index = {}
+
+        while end < len(nums):
+            curr_num = nums[end]
+            last_occurrence = num_to_index.get(curr_num, -1)
+            # if current window already has number or if window is too big, adjust window
+            while begin <= last_occurrence or end - begin + 1 > k:
+                current_sum -= nums[begin]
+                begin += 1
+            num_to_index[curr_num] = end
+            current_sum += nums[end]
+            if end - begin + 1 == k:
+                ans = max(ans, current_sum)
+            end += 1
+        return ans     
