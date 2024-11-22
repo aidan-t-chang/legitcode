15714fa24b73338e4ac06f75b26b137512b237e5
plinygg <aidan.chang@outlook.com>
2024-11-22 17:16:56 -0600

    solved daily problem 11/22/24

---
+++ daily problems/11.11-flipcolumnsformaximumnumberofequalrows.py
@@ -0,0 +1,19 @@
+# You are given an m x n binary matrix matrix.
+
+# You can choose any number of columns in the matrix and flip every cell in that column 
+# (i.e., Change the value of the cell from 0 to 1 or vice versa).
+
+# Return the maximum number of rows that have all values equal after some number of flips.
+
+class Solution:
+    def maxEqualRowsAfterFlips(self, matrix) -> int:
+        count = defaultdict(int)
+
+        for row in matrix:
+            rowk = tuple(row)
+
+            if row[0]:
+                rowk = tuple([0 if n else 1 for n in row])
+            count[rowk] += 1
+
+        return max(count.values())
