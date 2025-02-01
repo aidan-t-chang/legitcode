c5ffe884e582d8cf062f24e67f6f560eceb4f273
plinygg <aidan.chang@outlook.com>
2025-02-01 16:01:45 -0600

    solved daily problem 2/1/25

--- daily problems-2025/02.01-specialarrayi.py
+++ daily problems-2025/02.01-specialarrayi.py
@@ -3,7 +3,7 @@
 # You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
 
 class Solution:
-    def isArraySpecial(self, nums: List[int]) -> bool:
+    def isArraySpecial(self, nums) -> bool:
         for i in range(len(nums)-1):
             if (nums[i] % 2 and nums[i+1] % 2) or (not nums[i] % 2 and not nums[i+1] % 2):
                 return False
