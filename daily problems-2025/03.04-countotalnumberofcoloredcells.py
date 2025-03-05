91f4e14ab51efb792bd5d022e9f9edbfcbf2fbca
plinygg <aidan.chang@outlook.com>
2025-03-04 23:16:34 -0600

    solved daily problem 3/4/25

---
+++ daily problems-2025/03.04-countotalnumberofcoloredcells.py
@@ -0,0 +1,11 @@
+# There exists an infinitely large two-dimensional grid of uncolored unit cells. 
+# You are given a positive integer n, indicating that you must do the following routine for n minutes:
+
+# At the first minute, color any arbitrary unit cell blue.
+# Every minute thereafter, color blue every uncolored cell that touches a blue cell.
+# Below is a pictorial representation of the state of the grid after minutes 1, 2, and 3.
+
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        # adds two in each direction, then add 2 on top of that 
+        return n**2 + (n-1)**2
