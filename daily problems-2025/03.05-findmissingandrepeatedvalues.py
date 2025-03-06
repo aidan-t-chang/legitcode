840370bfbcb3869fd8434ba5df5d02c11f7dc318
plinygg <aidan.chang@outlook.com>
2025-03-06 16:45:54 -0600

    solved daily problem 3/6/25

---
+++ daily problems-2025/03.05-findmissingandrepeatedvalues.py
@@ -0,0 +1,21 @@
+# You are given a 0-indexed 2D integer matrix grid of size n * n with values in the range [1, n2]. 
+# Each integer appears exactly once except a which appears twice and b which is missing. 
+# The task is to find the repeating and missing numbers a and b.
+
+# Return a 0-indexed integer array ans of size 2 where ans[0] equals to a and ans[1] equals to b.
+
+class Solution:
+    def findMissingAndRepeatedValues(self, grid):
+        n = len(grid)
+        total = n**2
+
+        sumval = sum(num for row in grid for num in row)
+        sqrsum = sum(num**2 for row in grid for num in row)
+
+        diff = sumval - total * (total + 1) // 2
+        diffsqr = sqrsum - total * (total + 1) * (2 * total + 1) // 6
+
+        repeat = (diffsqr // diff + diff)// 2
+        missing = (diffsqr // diff - diff) // 2
+
+        return [repeat, missing]
