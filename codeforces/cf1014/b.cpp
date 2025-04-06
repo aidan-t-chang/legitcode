6a39d93c6863ad2367b46629cdaf3d12e186544b
Aidan Chang <119556552+plinygg@users.noreply.github.com>
2025-04-06 12:44:34 -0500

    solved cf1014 lady bug

---
+++ codeforces/cf1014/b.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    string a, b; cin >> a >> b;
+    int cnt1 = 0, cnt2 = 0;
+    for (int i = 0; i < n; ++i) {
+        if (i & 1) {
+            cnt2 += a[i] == '0';
+            cnt1 += b[i] == '0';
+        } else {
+            cnt1 += a[i] == '0';
+            cnt2 += b[i] == '0';
+        }
+    }
+    cout << (cnt1 >= (n + 1) / 2 && cnt2 >= n / 2 ? "Yes" : "No") << '\n';
+}
+
+int main() {
+    int t; cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
