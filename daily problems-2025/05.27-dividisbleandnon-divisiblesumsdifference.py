49263a412149c8aef2ff53535d9bb750165b02f8
Aidan Chang <119556552+plinygg@users.noreply.github.com>
2025-05-27 15:51:29 -0500

    solved daily problem 5/27/25

---
+++ daily problems-2025/05.27-dividisbleandnon-divisiblesumsdifference.py
@@ -0,0 +1,20 @@
+# You are given positive integers n and m.
+
+# Define two integers as follows:
+
+# num1: The sum of all integers in the range [1, n] (both inclusive) that are not divisible by m.
+# num2: The sum of all integers in the range [1, n] (both inclusive) that are divisible by m.
+# Return the integer num1 - num2.
+
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        yes = 0
+        no = 0
+
+        for n in range(1, n+1):
+            if not n % m:
+                no += n
+            else:
+                yes += n
+
+        return yes - no
